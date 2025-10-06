d674915e39cd1d498812a0cc5f57b0587ea307f9
Sultan Mahmud <kaziiriad@gmail.com>
2025-10-06 19:43:54 +0600

    fix: Modified apps.py for proper module reference.

--- backend/invoicing/apps.py
+++ backend/invoicing/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class InvoicingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'invoicing'
+    name = 'backend.invoicing'
